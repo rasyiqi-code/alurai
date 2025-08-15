93968d1ed25b7f22c6ec3b771ab4a14a0ec5a6d3
Retas Lintas Batas <retaslintasbatas@gmail.com>
2025-08-15 14:09:25 +0000

    tinggi headernya kecilkan

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { Logo } from '@/components/icons/logo';
 
 export function Header() {
   return (
-    <header className="p-4 border-b bg-card">
+    <header className="p-2 border-b bg-card">
       <div className="container mx-auto flex items-center gap-3">
         <Logo />
         <h1 className="text-xl font-bold tracking-tight text-foreground">
