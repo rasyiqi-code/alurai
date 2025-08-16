42e9a500a8593a534ceb37bf29157753d49f5e29
Retas Lintas Batas <retaslintasbatas@gmail.com>
2025-08-16 15:33:24 +0000

    saya coba yang tanpa upload file sukses tidak ada error, tapi gak masuk

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,10 +4,9 @@ import { initializeApp, getApps } from "firebase/app";
 const firebaseConfig = {
   "projectId": "formflow-ai-966kl",
   "appId": "1:584895579440:web:826a8d9f7fddea4756f4f5",
-  "storageBucket": "formflow-ai-966kl.firebasestorage.app",
+  "storageBucket": "formflow-ai-966kl.appspot.com",
   "apiKey": "AIzaSyBnjIkax1IaFr2YA5cCtmdDs4qySi0kMh0",
   "authDomain": "formflow-ai-966kl.firebaseapp.com",
-  "measurementId": "",
   "messagingSenderId": "584895579440"
 };
 
